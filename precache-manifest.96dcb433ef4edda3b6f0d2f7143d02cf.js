08b0377fe81b6577dbe440bea470f4f2df9c15a2
rahul020800 <63282446+rahul020800@users.noreply.github.com>
2020-12-03 10:01:48 +0530

    Updates

---
+++ precache-manifest.96dcb433ef4edda3b6f0d2f7143d02cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f27d35087a026afa66697a2f5c77174",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "057404a55b3c99d292bc",
+    "url": "/covid-19/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "db48d3984dadbe5d4307",
+    "url": "/covid-19/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "057404a55b3c99d292bc",
+    "url": "/covid-19/static/js/2.f5be4388.chunk.js"
+  },
+  {
+    "revision": "b29fa9d568ccdfac989d0d2ab5fd3892",
+    "url": "/covid-19/static/js/2.f5be4388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db48d3984dadbe5d4307",
+    "url": "/covid-19/static/js/main.4122f7dc.chunk.js"
+  },
+  {
+    "revision": "82824c26418798f66e6d",
+    "url": "/covid-19/static/js/runtime-main.33bc695f.js"
+  }
+]);
